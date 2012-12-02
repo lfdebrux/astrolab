f9bf9672e0608e3e1077d9987f43d9a2ab20ff8c
Laurence de Bruxelles <hiktout@gmail.com>
2012-12-02 20:48:27 +0000

    added a test of .set_observer() memoization

--- test_store.py
+++ test_store.py
@@ -6,10 +6,12 @@ t = time.strftime('%Y-%m-%dT%H:%M:%S') # current time in ISO format
 
 jpl.new_session()
 
+jpl.set_observer(True)
 jpl.set_target(target)
 jpl.set_time(t)
 
 
 t = time.strftime('%Y-%m-%dT%H:%M:%S')
+jpl.set_observer(True)
 jpl.set_target(target)
 jpl.set_time(t)
