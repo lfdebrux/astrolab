ce6430ba89c2649fbc43eae279dae7131706ec8f
Laurence de Bruxelles <hiktout@gmail.com>
2012-12-04 21:40:04 +0000

    added script get_fwhm.sh which uses starlink packages to find the psf of a target

---
+++ get_fwhm.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+star_dir="/star"
+
+source ${star_dir}/etc/profile
+
+fits=$1
+ra=$2
+dec=$3
+
+base="`basename "$fits" .fits`"
+ndf=${base}.sdf
+
+# load necessary packages
+convert > /dev/null
+kappa > /dev/null
+
+# convert file to ndf
+fits2ndf $fits $ndf
+
+# convert position to expected format
+#listmake ${base}_pos.txt ndf=$ndf mode="Interface" position="$ra $dec"
+
+# put positions in a file
+echo $ra $dec > pos.txt
+
+# find psf
+psf $ndf incat=! device=! cofile=pos.txt > /dev/null
+
+parget fwhm psf
+
+rm pos.txt $sdf
