b3eb23ad4d8e5dbf9e47143095ce928b3be51480
Laurence de Bruxelles <hiktout@gmail.com>
2012-12-05 12:46:49 +0000

    Corrected references to find_jpl_position

--- test_store.py
+++ test_store.py
@@ -1,5 +1,5 @@
 import time
-import find_jpl_position as jpl
+import get_jpl_position as jpl
 
 target = 'niobe'
 t = time.strftime('%Y-%m-%dT%H:%M:%S') # current time in ISO format
@@ -9,7 +9,6 @@ jpl.new_session()
 jpl.set_observer(True)
 jpl.set_target(target)
 jpl.set_time(t)
-
 
 t = time.strftime('%Y-%m-%dT%H:%M:%S')
 jpl.set_observer(True)
