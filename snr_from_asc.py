5a15c9218d0a1920709e042f2bf0a6b2c9a0c477
Laurence de Bruxelles <hiktout@gmail.com>
2012-12-05 10:46:17 +0000

    forgot to close open()ed file, fixed now

---
+++ snr_from_asc.py
@@ -0,0 +1,25 @@
+import sys
+
+isofluxes = []
+isonoises = []
+autofluxes = []
+autonoises = []
+
+with open(sys.argv[1]) as f:
+	for line in f:
+		if '#' not in line:
+			data = line.split()
+			
+			#print data
+			isofluxes.append(float(data[3]))
+			isonoises.append(float(data[4]))
+			autofluxes.append(float(data[18]))
+			autonoises.append(float(data[19]))
+
+isosnrs = map(lambda x,y: x/y, isofluxes, isonoises)
+autosnrs = map(lambda x,y: x/y, autofluxes, autonoises)
+
+n = int(sys.argv[2]) - 1
+
+print 'Isotopal Flux SNR', isosnrs[n]
+print 'Auto Flux SNR', autosnrs[n]
