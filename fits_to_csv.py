ae8f8bb10fdc054f395364e6775207c316bfab0c
Laurence de Bruxelles <hiktout@gmail.com>
2012-12-05 10:55:30 +0000

    refactored a lot, now gets most details from functions in get_fwhm_snr_from_asc, also more robut try:finally clause used in main loop to make sure we always clean up

--- fits_to_csv.py
+++ fits_to_csv.py
@@ -4,63 +4,25 @@ import find_jpl_position as jpl
 import os, sys, subprocess, StringIO
 
 from utils import *
+from get_fwhm_snr_from_asc import run_sextractor,find_target_in_catalogue
 
-output_filename = sys.argv[-1]
-
-if not '.csv' in output_filename:
-	raise ValueError('Write file is not a csv!')
-
-output = open(output_filename, "w")
-headers = "Object,Date,Time,Ast.RA,Ast.Dec,JPL RA,JPL Dec,RA Delta,Dec Delta,Astrom Number,Astrom RMS"
-output.write(headers)
-output.write("\n")
-
-cwd = os.getcwd()
-
-jpl.new_session()
-
-for file in sys.argv[1:-1]:
-	if not os.path.dirname(file) is '':
-		os.chdir(os.path.dirname(file))
-		
-	fits = os.path.basename(file)
-	
-	target,date_time = get_obs_details(file)
-	print target, date_time, '|',
-	
-	ra_jpl,dec_jpl = get_jpl_ra_dec(target,date_time)
-	print ra_jpl, dec_jpl, '|',
-	
-	ra_astrom,dec_astrom = get_astrom_ra_dec(fits)
-	
-	ra_delta = str(delta_ra(ra_jpl,ra_astrom))
-	dec_delta = str(delta_dec(dec_jpl,dec_astrom))
-	
-	fwhm = get_fwhm(file,ra_astrom,dec_astrom)
-	
-	ra_astrom, dec_astrom, ra_jpl, dec_jpl = map(' '.join,(ra_astrom, dec_astrom, ra_jpl, dec_jpl))
-	
-	print ra_astrom, dec_astrom, '|',
-	print astrom_number, '|',
-	print astrom_rms, '|', fwhm
-	
-	date, time = date_time.split('T')
-	
-	os.chdir(cwd)
-	
-	row = ','.join((target,date,time,ra_astrom,dec_astrom,ra_jpl,dec_jpl,ra_delta,dec_delta,astrom_number,astrom_rms))
-	output.write(row)
-	output.write("\n")
-
-output.close()
+astrom_command = '/remote/aa_64bin/auto_astrom/ucac4_find_astrom.py'
 
 def get_obs_details(fits):
+	"""
+	Find the name of the target in a fits file and the time and date of when
+	the observation was made using gethead from WCSTools.
+	"""
 	target = subprocess.check_output(['gethead','OBJ_NAME',fits]).strip()
 	date_time = subprocess.check_output(['gethead','DATE-OBS',fits]).strip()
 	
 	return target,date_time
 
 def get_jpl_ra_dec(target,date_time):
+	"""
+	Find the position according to JPL Horizons for a target at a set
+	data and time, date_time, and return as RA and Dec in tuple form
+	"""
 	jpl.set_target(target)
 	jpl.set_time(date_time)
 	
@@ -70,40 +32,19 @@ def get_jpl_ra_dec(target,date_time):
 	
 	return ra_jpl,dec_jpl
 
-def get_astrom_ra_dec(fits):
-	astrom_command = '/remote/aa_64bin/auto_astrom/ucac4_find_astrom.py '
-	astrom_command += fits
-	astrom_command += ' | '
-	astrom_command += "sed -n -e '/Got/p' -e '/Unknowns, 6/,/finished/p' -e '/RMS :/p'"
+def get_astrom_ra_dec_snr_fwhm(fits,ra,dec):
+	"""
+	Get the information on the target by running the sextractor
+	and reading the resultant catalogue
+	"""
+	try:
+		catalogue = run_sextractor(fits)
+		ra_astrom,dec_astrom,snr,fwhm = find_target_in_catalogue(catalogue,ra,dec)
+	except RuntimeError:
+		print 'Target Not Found in ', fits
+		raise RuntimeError('Target Not Found in' + fits)
 	
-	astrom_new = StringIO.StringIO(subprocess.check_output(astrom_command, shell=True))
-	
-	cleanup(os.getcwd())
-	
-	i=0
-	for line in astrom_new:
-		if 'Got' in line:
-			astrom_number = line.split()[1]
-		if 'RMS :' in line:
-			i+=1
-			if i == 4:
-				astrom_rms = line.split()[2]
-		if '->' in line:
-			ra_astrom = line.split()[3:6]
-			dec_astrom = line.split()[6:10]
-			
-			#print ra_astrom, dec_astrom,
-			
-			if ra_astrom[0:2] == ra_jpl[0:2] and dec_astrom[0:2] == dec_jpl[0:2]:
-				if ra_astrom[2][:1] == ra_jpl[2][:1] and dec_astrom[2][:1] == dec_jpl[2][:1]:
-					break
-	else:
-		#raise RuntimeError('No match for the asteroid found in {}!'.format(file))
-		print 'Target Not Found', file
-		os.chdir(cwd)
-		continue
-	
-	return ra_astrom,dec_astrom
+	return ra_astrom,dec_astrom,snr,fwhm
 
 def get_fwhm(fits,ra,dec):
 	ra = ':'.join(ra)
@@ -112,3 +53,54 @@ def get_fwhm(fits,ra,dec):
 	fwhm = subprocess.check_output(['/home/astrolab/matt_laurence/get_fwhm.sh',fits,ra,dec])
 	
 	return fwhm
+
+if __name__ == '__main__':
+
+	output_filename = sys.argv[-1]
+
+	if not '.csv' in output_filename:
+		raise ValueError('Write file is not a csv!')
+
+	output = open(output_filename, "w")
+	headers = "Object,Date,Time,Ast.RA,Ast.Dec,JPL RA,JPL Dec,RA Delta,Dec Delta,Astrom Number,Astrom RMS,SNR,FWHM"
+	output.write(headers)
+	output.write("\n")
+
+	cwd = os.getcwd()
+
+	jpl.new_session()
+
+	for file in sys.argv[1:-1]:	
+		try:
+			if not dir is '':
+				os.chdir(dir)
+		
+			fits = os.path.basename(file)
+
+			target,date_time = get_obs_details(fits)
+			ra_jpl,dec_jpl = get_jpl_ra_dec(target,date_time)
+			ra_astrom,dec_astrom,snr,fwhm = get_astrom_ra_dec_snr_fwhm(fits)
+		else:
+			ra_delta = str(delta_ra(ra_jpl,ra_astrom))
+			dec_delta = str(delta_dec(dec_jpl,dec_astrom))
+			# fwhm = str(get_fwhm(fits,ra_astrom,dec_astrom))
+			date, time = date_time.split('T')
+			
+			ra_astrom, dec_astrom, ra_jpl, dec_jpl = map(' '.join,(ra_astrom, dec_astrom, ra_jpl, dec_jpl))
+		
+			print target, date_time, '|',
+			print ra_jpl, dec_jpl, '|',
+			print ra_astrom, dec_astrom, '|',
+			print astrom_number, '|',
+			print astrom_rms, '|', 
+			print snr, '|',
+			print fwhm
+
+			row = ','.join((target,date,time,ra_astrom,dec_astrom,ra_jpl,dec_jpl,ra_delta,dec_delta,astrom_number,astrom_rms,snr,fwhm))
+			output.write(row)
+			output.write("\n")
+		finally:
+			# always cleanup and chdir
+			cleanup(os.getcwd())
+			output.close()
+			os.chdir(cwd)
