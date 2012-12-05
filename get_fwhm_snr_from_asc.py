992d026b551ce6fec383c6c59319fed640a69963
Laurence de Bruxelles <hiktout@gmail.com>
2012-12-05 04:00:48 +0000

    now works

--- get_fwhm_snr_from_asc.py
+++ get_fwhm_snr_from_asc.py
@@ -1,7 +1,7 @@
 import os,subprocess,sys
 from math import sqrt
 
-from jrl_utils import deg2dms,deg2hms
+# from jrl_utils import deg2dms,deg2hms
 
 def find_target_in_catalogue(catalogue,ra,dec):
 	file = open(catalogue)
@@ -13,10 +13,11 @@ def find_target_in_catalogue(catalogue,ra,dec):
 		ra_cat,dec_cat = map(float,line.split()[1:3])
 
 		if round(ra-ra_cat,2) == 0 and round(dec-dec_cat,2) == 0:
-			peak,background,fwhm = line.split()[7:]
-			ra_cat = deg2hms(ra_cat)
-			dec_cat = deg2dms(dec_cat)
-			snr = get_snr(peak/bg)
+			peak,background,fwhm = map(float,line.split()[7:])
+			# ra_cat = deg2hms(ra_cat)
+			# dec_cat = deg2dms(dec_cat)
+			fwhm = fwhm * 3600
+			snr = get_snr(peak,background)
 			return ra_cat,dec_cat,snr,fwhm
 	else:
 		raise RuntimeError('No match for the asteroid found')
@@ -33,7 +34,7 @@ if __name__ == '__main__':
 	from utils import ra2deg,dec2deg
 
 	sextractor = os.path.join(os.environ['STARLINK_DIR'],'bin/extractor/sex')
-	config_file = '/Users/laurence/Coding/astrolab/ascfit.sex'
+	config_file = './ascfit.sex'
 
 	fits = sys.argv[1]
 	ra = ra2deg(map(float,sys.argv[2].split(':')))
@@ -41,4 +42,4 @@ if __name__ == '__main__':
 
 	subprocess.call([sextractor,'-c',config_file,fits])
 
-	print find_target_in_catalogue('asc_sextr.cat')
+	print find_target_in_catalogue('asc_sextr.cat',ra,dec)
