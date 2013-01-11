d8fb46a215e4b97c43be8ec0b46b8deb977ff45d
Laurence de Bruxelles <hiktout@gmail.com>
2013-01-11 02:25:47 +0000

    Try figaro instead of kappa

--- curate.sh
+++ curate.sh
@@ -5,19 +5,21 @@ STARLINK_DIR=${STARLINK_DIR:-/star}
 source ${STARLINK_DIR}/etc/profile
 
 # load starlink packages
+figaro > /dev/null
 convert > /dev/null
-kappa > /dev/null
 
 # create a display window
-gdset xwindows
-xmake xwindows -width 800 -height 600
+#gdset xwindows
+#xmake xwindows -width 800 -height 600
+idev xw
 
 for fits in "$@"
 do
 	echo $fits
 
 	# display image
-	display $fits noaxes mode=faint clear=true > /dev/null
+	#display $fits noaxes mode=faint clear=true > /dev/null
+	image $fits erase=true accept > /dev/null
 
 	# query user
 	read -n1 -p 'Keep? [Y/n]: ' keep
